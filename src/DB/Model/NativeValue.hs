c740e6d9abb9fbc88856be8ff0933dc666f52386
Rune K. Svendsen <runesvend@gmail.com>
2016-11-21 21:04:04 +0100

    cleanup

--- src/DB/Model/NativeValue.hs
+++ src/DB/Model/NativeValue.hs
@@ -17,21 +17,11 @@ import           Data.ByteString    (ByteString)
 import           Data.Int           (Int64)
 import           Data.Maybe         (fromMaybe)
 import           Data.Typeable
-import           Data.Scientific    (Scientific)
 
 
 -- | A class for the value types inside a Datastore object
 class Typeable a => NativeValue a where
     valueLens       :: Lens' DS.Value (Maybe a)
-    encode          :: a -> DS.Value
-    decode          :: DS.Value -> a
-    decodeMaybe     :: DS.Value -> Maybe a
-
-    encode a = set valueLens (Just a) value
-    decodeMaybe = view valueLens
-    decode v = fromMaybe
-        ( error $ show (typeOf (undefined :: a)) ++ " not present in DS.Value: " ++ show v )
-        (decodeMaybe v)
 
 instance NativeValue Bool            where valueLens = vBooleanValue
 instance NativeValue ByteString      where valueLens = vBlobValue
@@ -43,3 +33,13 @@ instance NativeValue DS.Entity       where valueLens = vEntityValue
 instance NativeValue DS.ArrayValue   where valueLens = vArrayValue
 instance NativeValue DS.ValueNullValue where valueLens = vNullValue
 
+encode :: NativeValue a => a -> DS.Value
+encode a = set valueLens (Just a) value
+
+decodeMaybe :: NativeValue a => DS.Value -> Maybe a
+decodeMaybe = view valueLens
+
+decode :: forall a. NativeValue a => DS.Value -> a
+decode v = fromMaybe
+    ( error $ show (typeOf (undefined :: a)) ++ " not present in DS.Value: " ++ show v )
+    (decodeMaybe v)
